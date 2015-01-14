44692ae2d47569c8cf9346304fe28d00c57c952c
Etienne Cuvillon <etienne.cuvillon@gmail.com>
2015-01-14 05:05:50 +0100

    Second grammar done
    Grade A/B, parser and evaluate done.

--- interpreter.pl
+++ interpreter.pl
@@ -1,6 +1,6 @@
 /***
 An Interpreter for Assignment 3 on PROP HT2014 at DSV/SU.
-First grammar, grade C/D/E
+Second grammar, grade A/B
 Loric Brevet
 Etienne Cuvillon
 ***/
@@ -74,14 +74,15 @@ parse(-ParseTree)-->
 	and returning a parse tree.
 ***/
 
-parse(ParseTree) --> assignment(ParseTree).
-/*
+parse(ParseTree) --> block(ParseTree).
+
 block(block(Left_curly,Stmts,Right_curly)) -->
 	left_curly(Left_curly), statements(Stmts), right_curly(Right_curly).
 
 statements(statements(Assign,Stmts)) -->
-	assignment(Assign), statements(Stmts).
-*/
+	assignment(Assign), !, statements(Stmts).
+statements(statements) --> !.
+
 assignment(assignment(Id,Eq,Expr,Sem)) -->
 	ident(Id), assign_op(Eq), expression(Expr), semicolon(Sem).
 
@@ -101,6 +102,8 @@ term(term(Factor,Div_op,Term)) -->
 
 factor(factor(Int)) -->
 	int(Int).
+factor(factor(Ident)) -->
+	ident(Ident).
 factor(factor(Left_paren, Expr, Right_paren)) -->
 	left_paren(Left_paren), expression(Expr), right_paren(Right_paren).
 
@@ -113,8 +116,8 @@ mult_op(mult_op) --> [*].
 div_op(div_op) --> [/].
 left_paren(left_paren) --> ['('].
 right_paren(right_paren) --> [')'].
-/*left_curly(left_curly) --> ['{'].
-right_curly(right_curly) --> ['}'].*/
+left_curly(left_curly) --> ['{'].
+right_curly(right_curly) --> ['}'].
 semicolon(semicolon) --> [;].
 
 /***
@@ -124,29 +127,61 @@ evaluate(+ParseTree,+VariablesIn,-VariablesOut):-
 	their values.
 ***/
 
-evaluate(ParseTree,_,VariablesOut):-
-	ParseTree = assignment(ident(Id),assign_op,Expr,semicolon),
-	VariablesOut = [Id = Res],
-	evaluate_expr(Expr,nil,0,Res).
+evaluate(ParseTree,VariablesIn,VariablesOut):-
+	ParseTree = block(left_curly,Stmts,right_curly),
+	evaluate_stmts(Stmts,VariablesIn,VariablesOut).
 
-evaluate_expr(expression(Term),Op,Prec,Res):-
-	evaluate_term(Term,nil,0,Res2), eval(Prec,Res2,Op,Res).
-evaluate_expr(expression(Term,Op1,Expr),Op2,Prec,Res):-
-	evaluate_term(Term,nil,0,Res2), evaluate_expr(Expr,Op1,Res3,Res),
+evaluate_stmts(statements,Variables,Variables).
+evaluate_stmts(statements(Assign,Stmts),VariablesIn,VariablesOut):-
+	evaluate_assign(Assign,VariablesIn,VariablesNew),
+	evaluate_stmts(Stmts,VariablesNew,VariablesOut).
+
+evaluate_assign(Assign,VariablesIn,VariablesNew):-
+	Assign = assignment(ident(Id),assign_op,Expr,semicolon),
+	evaluate_expr(Expr,nil,0,Res,VariablesIn),
+	add_list(Id = Res,VariablesIn,VariablesNew).
+
+evaluate_expr(expression(Term),Op,Prec,Res,VariablesIn):-
+	evaluate_term(Term,nil,0,Res2,VariablesIn),
+	eval(Prec,Res2,Op,Res).
+evaluate_expr(expression(Term,Op1,Expr),Op2,Prec,Res,VariablesIn):-
+	evaluate_term(Term,nil,0,Res2,VariablesIn),
+	evaluate_expr(Expr,Op1,Res3,Res,VariablesIn),
 	eval(Prec,Res2,Op2,Res3).
 
-evaluate_term(term(Factor),Op,Prec,Res):-
-	evaluate_factor(Factor,Res2), eval(Prec,Res2,Op,Res).
-evaluate_term(term(Factor,Op1,Term),Op2,Prec,Res):-
-	evaluate_factor(Factor,Res2), evaluate_term(Term,Op1,Res3,Res),
+evaluate_term(term(Factor),Op,Prec,Res,VariablesIn):-
+	evaluate_factor(Factor,Res2,VariablesIn),
+	eval(Prec,Res2,Op,Res).
+evaluate_term(term(Factor,Op1,Term),Op2,Prec,Res,VariablesIn):-
+	evaluate_factor(Factor,Res2,VariablesIn),
+	evaluate_term(Term,Op1,Res3,Res,VariablesIn),
 	eval(Prec,Res2,Op2,Res3).
 
-evaluate_factor(factor(int(Int)),Int).
-evaluate_factor(factor(left_paren,Expr,right_paren), Res) :-
-	evaluate_expr(Expr,nil,0,Res).
+evaluate_factor(factor(int(Int)),Int,_).
+evaluate_factor(factor(ident(Ident)),Res,VariablesIn):-
+	find(VariablesIn,Ident,Res).
+evaluate_factor(factor(left_paren,Expr,right_paren), Res,VariablesIn):-
+	evaluate_expr(Expr,nil,0,Res,VariablesIn).
 
 eval(X,Y,add_op,X+Y).
 eval(X,Y,sub_op,X-Y).
 eval(X,Y,mult_op,X*Y).
 eval(X,Y,div_op,X/Y).
 eval(_,Y,nil,Y).
+
+/*
+add_list(+Elem, +List1, -List2)
+List2 is List1 with Elem added in.
+*/
+add_list(Elem, List1, [Elem|List1]).
+
+/*
+find(+Vars,+Ident,-Value)
+Vars is a list of variables. If the variable Ident is in Vars then Value
+is the value of Ident (Ident = Value), otherwise Ident = 0.
+*/
+find([],_,0).
+find([Id = Value|_],Id,Value):-!.
+find([Id1 = _|Y],Id2,Res):-
+	Id1 \== Id2,
+	find(Y,Id2,Res).
